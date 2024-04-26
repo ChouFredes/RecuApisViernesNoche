7a8ed3b370c54b608ba2d6b0f1e240da1b5db653
JuanmaGrispo <juanmanuelgrispo@gmail.com>
2024-04-25 23:04:37 -0300

    last productCard update 25/04

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -11,14 +11,17 @@ export function ProductCard() {
       <div className="ps-productCard-details">
         
         <h3 className="ps-productCard-price">
-           1500 usd</h3>
+           $1500</h3>
         <header className="ps-productCard-description">
           Macbook pro 16 inches
         </header>
-        
+
       </div>
 
-      <button> Buy Item </button>
+      <div className="ps-productCard-actions">
+        <div className="ps-productCard-productRate"> 5 stars </div>
+        <button className="ps-productCard-buyButton"> Buy Item </button>
+      </div>
     </div>
   );
 }

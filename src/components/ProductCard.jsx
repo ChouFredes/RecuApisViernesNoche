80798a0295118ca9be0c7b580fc79dff178f6543
JuanmaGrispo <juanmanuelgrispo@gmail.com>
2024-04-25 22:49:39 -0300

    productCard img with background and details => price and description

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,24 @@
+export function ProductCard() {
+  return (
+    <div className="ps-productCard">
+      <div className="ps-productCard-img-container">
+        <img
+          src="src/assets/macbookPro.png"
+          alt="Imagen del producto"
+          className="ps-productCard-img"
+        />
+      </div>
+      <div className="ps-productCard-details">
+        
+        <h3 className="ps-productCard-price">
+           1500 usd</h3>
+        <header className="ps-productCard-description">
+          Macbook pro 16 inches
+        </header>
+        
+      </div>
+
+      <button> Buy Item </button>
+    </div>
+  );
+}
